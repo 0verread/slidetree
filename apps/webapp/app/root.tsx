a191a3131181573e21bf411ca11e00ed8c0b9343
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-04 16:52:24 -0400

    tailwidcss: Done :white_check_mark:

--- apps/webapp/app/root.tsx
+++ apps/webapp/app/root.tsx
@@ -9,8 +9,10 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "./tailwind.css";
+
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: styles },
 ];
 
 export default function App() {
@@ -22,7 +24,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-black">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
