0d094bbca886059f914fec3e99f28e019e6376e9
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-18 00:30:00 -0500

    add DocCard Components

--- apps/webapp/app/pages/Landing.tsx
+++ apps/webapp/app/pages/Landing.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from "react";
+import { DocCard } from "~/components/DocCard";
 
 
 const Landing: FC = () => {
   return (
-    <section className="px-2 sm:px-4 w-full z-20 top-0 left-0 flex items-center justify-center py-10 h-screen ">
+    <>
+    <section className="px-2 sm:px-4 w-full z-20 top-0 left-0 flex items-center justify-center py-10">
       <div className='bg-main text-center '>
         <div className='wrapper max-w-xl transform -translate-y-18 sm:-translate-y-2xfull '>
           <h1 className='text-white font-header font-bold text-5xl mb-6'>Open source alternative to Slideshare
@@ -12,6 +14,19 @@ const Landing: FC = () => {
         </div>
       </div>
     </section>
+    <div className="flex flex-row">
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+          <DocCard />
+          <DocCard />
+          <DocCard />
+          <DocCard />
+          <DocCard />
+          <DocCard />
+        </div>
+      </div>
+    </div>
+    </>
   )
 };
 
