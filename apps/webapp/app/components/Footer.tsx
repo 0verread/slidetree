e387ccea5f8e16641d0700a41b6ea22c275bf0a8
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-21 18:06:35 -0500

    Add Footer: WIP

---
+++ apps/webapp/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer : React.FC = () => {
+  return (
+    <div className='text-white flex text-center justify-center'>made with love</div>
+  )
+}
+
+export default Footer
--- apps/webapp/app/routes/_index.tsx
+++ apps/webapp/app/routes/_index.tsx
@@ -1,6 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 
 import { Header } from "../components/Header";
+import Footer from "~/components/Footer";
 import Landing from "~/pages/Landing";
 
 export const meta: MetaFunction = () => {
@@ -16,6 +17,7 @@ export default function Index() {
     <div className="mx-auto flex h-screen max-w-6xl flex-col">
       <Header />
       <Landing />
+      <Footer />
     </div>
     </>
   );
