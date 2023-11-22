e64b7ccde2048026739c8d72587c022dc4a87b3b
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-22 18:53:38 -0500

    few more lines

--- apps/webapp/app/routes/_index.tsx
+++ apps/webapp/app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 
-import { Header } from "../components/Header";
+import { Header } from "~/components/Header";
 import Footer from "~/components/Footer";
 import Landing from "~/pages/Landing";
 
