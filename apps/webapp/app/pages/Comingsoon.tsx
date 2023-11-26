f8a268b70aca77431681024a43b3addc05a34d5b
sub0ne1 <iamsubhajit.d@gmail.com>
2023-11-26 02:20:30 -0500

    changed bg:

--- apps/webapp/app/pages/Comingsoon.tsx
+++ apps/webapp/app/pages/Comingsoon.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Comingsoon = () => {
   return (
     <section className='px-2 sm:px-4 w-full z-20 top-0 left-0 flex items-center justify-center py-10 h-screen'>
-      <span className='text-white text-4xl'>Coming Soon</span>
+      <span className='text-zinc-300 text-5xl'>Coming Soon</span>
     </section>
   )
 }
--- apps/webapp/app/root.tsx
+++ apps/webapp/app/root.tsx
@@ -23,7 +23,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="bg-gradient-to-l from-gray-700 via-gray-900 to-black">
+      <body className="bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-black via-zinc-900 to-black">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
